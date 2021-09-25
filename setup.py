5ba5c1e13b60cb0b58830d0b2c3554881b8f18ba
max-fatouros <max.fatouros@gmail.com>
2021-09-25 17:34:21 -0500

    fixin stuffs

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="tensorflow_helper",
+    version="0.0.1",
+    author="Max Fatouors",
+    description="helper methods for tensorflow",
+    url="https://github.com/max-fatouros/tensorflow_helpers",
+
+    package_dir={"": "tensorflow_helpers"},
+    packages=setuptools.find_packages(where="tensorflow_helpers"),
+    python_requires=">=3.6",
+)
