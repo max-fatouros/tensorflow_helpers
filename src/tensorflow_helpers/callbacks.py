8070c30eae4b90225a8f8c2022db72c5ab8c877a
max-fatouros <max.fatouros@gmail.com>
2021-09-26 11:27:46 -0500

    Added EarlyStopping callback

--- src/tensorflow_helpers/callbacks.py
+++ src/tensorflow_helpers/callbacks.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+
 class EpochDots(tf.keras.callbacks.Callback):
     """Makes 'model.fit' print dots at each epoch and print the
     current epoch number in real time
@@ -22,7 +23,27 @@ class EpochDots(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
         epoch = epoch + 1  # epochs start counting at 0 for computer, this adds 1 for human
         if epoch % self.dots == 0:
-            print(f"\r{epoch}\t".expandtabs(4) + "."*self.dots, f"Loss: {logs}", end="\n")
+            print(f"\r{epoch}\t".expandtabs(4) + "." * self.dots, logs, end="\n")
         else:
-            print(f"\r{epoch}\t".expandtabs(4) + "."*(epoch % self.dots), end="")
+            print(f"\r{epoch}\t".expandtabs(4) + "." * (epoch % self.dots), end="")
 
+
+class EarlyStopping(tf.keras.callbacks.Callback):
+    def __init__(self, accuracy, val_accuracy):
+        super().__init__()
+        self.accuracy = accuracy
+        self.val_accuracy = val_accuracy
+
+    def on_epoch_end(self, epoch, logs={}):
+        if (logs.get('accuracy') is not None and logs.get('accuracy') >= self.accuracy and
+                logs.get('val_accuracy') is not None and logs.get('val_accuracy') >= self.val_accuracy):
+
+            if (logs.get('accuracy') and logs.get('val_accuracy')) is not None:
+                print(f"\nEnding training with an accuracy of {logs.get('accuracy')} "
+                      f"and a val_accuracy of {logs.get('val_accuracy')}")
+            elif logs.get('accuracy') is not None:
+                print(f"\nEnding training with an accuracy of {logs.get('accuracy')}")
+            elif logs.get('val_accuracy') is not None:
+                print(f"\nEnding training with a val_accuracy of {logs.get('val_accuracy')}")
+
+            self.model.stop_training = True
