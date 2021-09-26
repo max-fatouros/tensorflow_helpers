75dbbbb711652a0e5d169a7470166914388fd2e8
max-fatouros <max.fatouros@gmail.com>
2021-09-25 19:14:05 -0500

    got pip install git+... working, added some documentation to EpochDots

--- src/tensorflow_helpers/callbacks.py
+++ src/tensorflow_helpers/callbacks.py
@@ -1,19 +1,27 @@
-# import tensorflow as tf
-#
-# class Callback(tf.keras.callbacks.Callback):
-#     # def on_train_begin(self, epoch, logs=None):
-#     #     print("train_begin")
-#     def on_epoch_end(self, epoch, logs=None):
-#         epoch = epoch + 1  # epochs start counting at 0 for computer, this adds 1 for human
-#         dots = 50
-#         if epoch % dots == 0:
-#             print(f"\r{epoch}\t".expandtabs(4) + "."*dots, end="\n")
-#         else:
-#             print(f"\r{epoch}\t".expandtabs(4) + "."*(epoch % dots), end="")
-#
-# # Make sure to set verbose == 0
-#
-# #test commit
+import tensorflow as tf
 
-def add_one(number):
-    return number + 1
+class EpochDots(tf.keras.callbacks.Callback):
+    """makes 'model.fit' print dots at each epoch and print the
+    current epoch number in real time
+
+    eg.\n
+    5   ..... \n
+    7   ..
+
+    NOTE: Make sure to set verbose == 0
+    """
+
+    def __init__(self, dots):
+        """
+        Args:
+            dots: dots per line
+        """
+        super().__init__()
+        self.dots = dots
+
+    def on_epoch_end(self, epoch, logs=None):
+        epoch = epoch + 1  # epochs start counting at 0 for computer, this adds 1 for human
+        if epoch % dots == 0:
+            print(f"\r{epoch}\t".expandtabs(4) + "."*self.dots, end="\n")
+        else:
+            print(f"\r{epoch}\t".expandtabs(4) + "."*(epoch % self.dots), end="")
